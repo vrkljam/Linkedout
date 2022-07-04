e77d711beaae1884ad23caae93d41b4341969f92
vrkljam <vrkljam@earthlink.net>
2022-07-04 14:12:47 -0600

    made first model

--- linkoapp/models.py
+++ linkoapp/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    author=models.CharField(max_length=40)
+    title=models.CharField(max_length=150)
+    post_content=models.TextField()
+    post_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author
