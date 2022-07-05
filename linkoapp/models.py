851230cae4ba6231b11ba483d48d3e7065c2c727
vrkljam <vrkljam@earthlink.net>
2022-07-05 10:43:42 -0600

    added commments model

--- linkoapp/models.py
+++ linkoapp/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -10,3 +11,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.author
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=CASCADE)
+    author = models.CharField(max_length=50)
+    content=models.TextField()
+    created_date=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.author
+
+        
