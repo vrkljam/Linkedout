202f48704567d46fa4b7dac5cea9c10896f4db0c
vrkljam <vrkljam@earthlink.net>
2022-12-19 19:03:03 -0700

    temp fix

--- linkedout/settings.py
+++ linkedout/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'RENDER' not in os.environ 
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME') if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
