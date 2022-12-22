560b89c9f2d2596889c902683bc536338ef942bb
vrkljam <vrkljam@earthlink.net>
2022-12-22 15:53:10 -0700

    updating build sh file remove the 3 from python

--- build2.sh
+++ build2.sh
@@ -2,8 +2,8 @@
 
 # exit on error
 set -o errexit  
-pipen install
+pipenv install
 pipenv install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
-python3 manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
