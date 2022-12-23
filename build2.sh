76b986cd9304159ad3abc5dd5584daba8557d676
vrkljam <vrkljam@earthlink.net>
2022-12-22 17:38:08 -0700

    added 3 to python

--- build2.sh
+++ build2.sh
@@ -2,8 +2,8 @@
 
 # exit on error
 set -o errexit  
-pipenv install
-pipenv install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+pip install -r requirements.txt
+
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
