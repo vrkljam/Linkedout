23102e9bf6010ecc7258871ba1d85d6281b07057
vrkljam <vrkljam@earthlink.net>
2022-12-26 11:55:53 -0700

    removed 3 from build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --no-input
-python3 manage.py makemigrations
-python3 manage.py migrate
+python manage.py collectstatic --no-input
+
+python manage.py migrate
